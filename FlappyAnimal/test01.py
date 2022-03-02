e5979e9e1432bd15bd0628fbd8cbb18a1b75f184
DxPtl01 <87237633+DxPtl01@users.noreply.github.com>
2022-03-01 21:50:05 -0500

    Create test01.py

---
+++ FlappyAnimal/test01.py
@@ -0,0 +1,10 @@
+import unittest
+import app
+class Testing(unittest.TestCase):
+    def test1(self):
+        self.assertNotEqual(app.loginPage, 0)
+        
+    def test2(self):
+        self.assertIsNone(app.registerPage, msg = None)
+        
+unittest.main()
