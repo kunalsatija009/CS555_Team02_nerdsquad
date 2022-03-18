2f7d05bfa8ca1cdcfbf1a316a905191996f4696f
DxPtl01 <87237633+DxPtl01@users.noreply.github.com>
2022-03-17 23:44:58 -0400

    Sprint 02
    Sprint 02 - Game  Variable Settings

---
+++ FlappyAnimal/gameSettings.py
@@ -0,0 +1,27 @@
+
+# Screen Variables Setting
+TITLE = "The Flappy Animal Game"
+SCREENWIDTH = 1080  
+SCREEN_HEIGHT = 650  
+FPS = 60               # frame per second
+
+# Colors Variables Setting
+MEDUIMBLUE = (0, 0, 205)
+NAVYBLUE = (0, 0, 128)
+WHITE = (255, 255, 255)
+
+# Fonts Variables Setting
+BIGFONT = ("dejavusans", 100)
+MEDFONT = ("dejavusans", 50)
+SMALLFONT = ("dejavusans", 25)
+
+# Intialize of UserName and Score
+USERNAME = ""
+SCORE = ""
+NEWUSER = ""
+
+
+
+
+
+
