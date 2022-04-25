febbb564077c4dd22b40a741299f7c8a68ddad00
DxPtl01 <87237633+DxPtl01@users.noreply.github.com>
2022-04-24 22:45:24 -0400

    unittest
    unittest

--- FlappyAnimal/Test/Sprint04/test01.py
+++ FlappyAnimal/Test/Sprint04/test01.py
@@ -8,44 +8,44 @@ pygame.event.get()
 
 class Test(unittest.TestCase):
   def test01(self):
-    self.post_event = pygame.event.Event(pygame.MOUSEBUTTONDOWN, button = 1, pos = (10, 10))
-    pygame.event.post(self.post_event)
+    self.postEvent = pygame.event.Event(pygame.MOUSEBUTTONDOWN, button = 1, pos = (10, 10))
+    pygame.event.post(self.postEvent)
     self.event = pygame.event.poll()
     self.assertEqual(pygame.event.type == pygame.MOUSEBUTTONDOWN)
     self.assertEqual(pygame.event.button  == 1)
     self.assertEqual(pygame.event.pos  == (10, 10))
   
   def test02(self):
-    self.post_event = pygame.event.Event(pygame.KEYDOWN, {"key": pygame.K_1})
-    pygame.event.post(self.post_event)
+    self.postEvent = pygame.event.Event(pygame.KEYDOWN, {"key": pygame.K_1})
+    pygame.event.post(self.postEvent)
     self.event = pygame.event.poll()
     self.assertEqual(pygame.event.type == pygame.KEYDOWN)
     self.assertEqual(pygame.event.key  == pygame.K_1)
 
   def test03(self):
-    self.post_event = pygame.event.Event(pygame.KEYDOWN, {"key": pygame.K_2})
-    pygame.event.post(self.post_event)
+    self.postEvent = pygame.event.Event(pygame.KEYDOWN, {"key": pygame.K_2})
+    pygame.event.post(self.postEvent)
     self.event = pygame.event.poll()
     self.assertEqual(pygame.event.type == pygame.KEYDOWN)
     self.assertEqual(pygame.event.key  == pygame.K_2)
 
   def test04(self):
-    self.post_event = pygame.event.Event(pygame.KEYDOWN, {"key": pygame.K_3})
-    pygame.event.post(self.post_event)
+    self.postEvent = pygame.event.Event(pygame.KEYDOWN, {"key": pygame.K_3})
+    pygame.event.post(self.postEvent)
     self.event = pygame.event.poll()
     self.assertEqual(pygame.event.type == pygame.KEYDOWN)
     self.assertEqual(pygame.event.key  == pygame.K_3)
   
   def test05(self):
-    self.post_event = pygame.event.Event(pygame.KEYDOWN, {"key": pygame.K_4})
-    pygame.event.post(self.post_event)
+    self.postEvent = pygame.event.Event(pygame.KEYDOWN, {"key": pygame.K_4})
+    pygame.event.post(self.postEvent)
     self.event = pygame.event.poll()
     self.assertEqual(pygame.event.type == pygame.KEYDOWN)
     self.assertEqual(pygame.event.key  == pygame.K_4)
   
   def test06(self):
-    self.post_event = pygame.event.Event(pygame.KEYDOWN, {"key": pygame.K_p})
-    pygame.event.post(self.post_event)
+    self.postEvent = pygame.event.Event(pygame.KEYDOWN, {"key": pygame.K_p})
+    pygame.event.post(self.postEvent)
     self.event = pygame.event.poll()
     self.assertEqual(pygame.event.type == pygame.KEYDOWN)
     self.assertEqual(pygame.event.key  == pygame.K_p)
